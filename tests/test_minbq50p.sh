d7b520fcbefb9f77e97b248e536fc9fa703c0a1a
Andreas WILM <wilma@gis.a-star.edu.sg>
2017-06-13 22:54:44 +0800

    Release 0.0.12
    - Added dist/famas-0.0.12.tar.gz
    - New filter option --minbq50p
    - All trim values 0 by default, i.e no trimming, hence --no-filter
      option removed
    - Quality range and order check now always on, hence --no-order-check
      and no-order-check removed
    - Option -Q renamed to -5
    - Option -q renamed to -3
    - Added test fastq data to repo and added make check support

---
+++ tests/test_minbq50p.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#
+# test split functionality
+#
+
+
+source lib.sh || exit 1
+
+
+DEBUG=0
+i=../data/minbq50p_1.fq.gz
+j=../data/minbq50p_2.fq.gz
+odir=$(mktemp -d -t $0..sh.XXX) || exit 1
+o=$odir/o1.fastq.gz
+p=$odir/o2.fastq.gz
+
+
+cmd="$famas -i $i -j $j -o $o -p $p --quiet"
+if ! eval $cmd 2>log.txt; then
+    echoerror "The following command failed: $cmd"
+    exit 1
+fi
+num_out=$(zgrep -c '^@R' $o)
+if [ $num_out -ne 6 ]; then
+    echoerror "Expected 6 but got $num_out sequences from $cmd"
+    exit 1
+fi
+rm $o $p
+
+cmd="$famas -i $i -j $j -o $o -p $p -m 2 --quiet"
+if ! eval $cmd 2>log.txt; then
+    echoerror "The following command failed: $cmd"
+    exit 1
+fi
+num_out=$(zgrep -c '^@R' $o)
+if [ $num_out -ne 4 ]; then
+    echoerror "Expected 4 but got $num_out sequences from $cmd"
+    exit 1
+fi
+rm $o $p
+
+cmd="$famas -i $j -j $i -o $p -p $o -m 2 --quiet"
+if ! eval $cmd 2>log.txt; then
+    echoerror "The following command failed: $cmd"
+    exit 1
+fi
+num_out=$(zgrep -c '^@R' $o)
+if [ $num_out -ne 4 ]; then
+    echoerror "Expected 4 but got $num_out sequences from $cmd"
+    exit 1
+fi
+rm $o $p
+
+
+cmd="$famas -i $i -j $j -o $o -p $p -m 9 --quiet"
+if ! eval $cmd 2>log.txt; then
+    echoerror "The following command failed: $cmd"
+    exit 1
+fi
+num_out=$(zgrep -c '^@R' $o)
+if [ $num_out -ne 4 ]; then
+    echoerror "Expected 4 but got $num_out sequences from $cmd"
+    exit 1
+fi
+rm $o $p
+
+
+cmd="$famas -i $i -j $j -o $o -p $p -m 10 --quiet"
+if ! eval $cmd 2>log.txt; then
+    echoerror "The following command failed: $cmd"
+    exit 1
+fi
+num_out=$(zgrep -c '^@R' $o)
+if [ $num_out -ne 0 ]; then
+    echoerror "Expected 0 but got $num_out sequences from $cmd"
+    exit 1
+fi
+rm $o $p
+
+
+if [ $DEBUG -eq 1 ]; then
+    echodebug "Keeping $odir"
+else
+    test -d $odir && rm -rf $odir
+fi
